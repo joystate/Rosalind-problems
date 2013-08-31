185995911911d0b06057e91bc24882a2d4b5ed2a
Nadia <joystate@me.com>
2013-08-30 21:06:45 -0400

    Inferring mRNA from Protein

---
+++ mRNA_from_protein.py
@@ -0,0 +1,50 @@
+codon_table = {
+    'UUU': 'F',     'CUU': 'L',     'AUU': 'I',     'GUU': 'V',
+    'UUC': 'F',     'CUC': 'L',     'AUC': 'I',     'GUC': 'V',
+    'UUA': 'L',     'CUA': 'L',     'AUA': 'I',     'GUA': 'V',
+    'UUG': 'L',     'CUG': 'L',     'AUG': 'M',     'GUG': 'V',
+    'UCU': 'S',     'CCU': 'P',     'ACU': 'T',     'GCU': 'A',
+    'UCC': 'S',     'CCC': 'P',     'ACC': 'T',     'GCC': 'A',
+    'UCA': 'S',     'CCA': 'P',     'ACA': 'T',     'GCA': 'A',
+    'UCG': 'S',     'CCG': 'P',     'ACG': 'T',     'GCG': 'A',
+    'UAU': 'Y',     'CAU': 'H',     'AAU': 'N',     'GAU': 'D',
+    'UAC': 'Y',     'CAC': 'H',     'AAC': 'N',     'GAC': 'D',
+    'UAA': 'Stop',  'CAA': 'Q',     'AAA': 'K',     'GAA': 'E',
+    'UAG': 'Stop',  'CAG': 'Q',     'AAG': 'K',     'GAG': 'E',
+    'UGU': 'C',     'CGU': 'R',     'AGU': 'S',     'GGU': 'G',
+    'UGC': 'C',     'CGC': 'R',     'AGC': 'S',     'GGC': 'G',
+    'UGA': 'Stop',  'CGA': 'R',     'AGA': 'R',     'GGA': 'G',
+    'UGG': 'W',     'CGG': 'R',     'AGG': 'R',     'GGG': 'G'
+}
+ 
+file = '/Users/nadia/Downloads/rosalind_mrna (7).txt'
+ 
+ 
+def mrna(file):
+      
+    from collections import defaultdict
+    
+    f = open(file).read().strip()
+    
+    prot_to_ac = defaultdict(list)
+    for k, v in codon_table.iteritems():
+        prot_to_ac[v].append(k)
+      
+    all_rna_strings = []    
+      
+    for i in f:
+        all_rna_strings.append(prot_to_ac[i])
+    all_rna_strings.append(prot_to_ac['Stop'])
+    
+    total = 1
+    
+    for i in all_rna_strings:
+        total *= len(i)
+        
+    print total%1000000
+
+                     
+mrna(file)
+    
+      
+
